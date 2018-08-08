8ec9d5ffb2bfa16c4044129bcd69e95e50aa805f
Chu Song <chuchusong23@gmail.com>
2018-08-08 12:43:19 -0700

    Add hover effect to display a simple description of the project.

--- src/project/Project.js
+++ src/project/Project.js
@@ -8,30 +8,39 @@ class Project extends Component {
                 <h2 className="project-title">My Project</h2>
                 <div className="project-list">
                     <div className="project-item">
-                        <a href="https://viovo.herokuapp.com/" target="_blank" rel="noopener noreferrer" >
-                            <img className="project-image" src="https://res.cloudinary.com/chu327/image/upload/v1533709402/VioVo/v.png" alt="Viovo"/>
-                        </a>
-                        <br/>
                         <a className="project-link" href="https://viovo.herokuapp.com/" target="_blank" rel="noopener noreferrer" >
+                            <img className="project-image" src="https://res.cloudinary.com/chu327/image/upload/v1533709402/VioVo/v.png" alt="Viovo"/>
+                            <br/>
                             <h4 className="project-name">VioVo</h4>
+                            <div className="overlay">
+                                <p className="overlay-text">
+                                    A single-page front-end app that streams real-time most popular trending videos on YouTube and enables users to search and play selected videos  
+                                </p>
+                            </div>
                         </a>
                     </div>
                     <div className="project-item">
-                        <a  href="https://chattersquare.herokuapp.com/" target="_blank" rel="noopener noreferrer" >
-                            <img className="project-image chattersquare" src="https://res.cloudinary.com/chu327/image/upload/v1533709427/ChatterBox-UserProfilePics/G.png" alt="ChatterSqaure"/>
-                        </a>
-                        <br/>
-                        <a className="project-link" href="https://chattersquare.herokuapp.com/" target="_blank" rel="noopener noreferrer" >
+                        <a  className="project-link" href="https://chattersquare.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                            <img className="project-image" src="https://res.cloudinary.com/chu327/image/upload/v1533709427/ChatterBox-UserProfilePics/G.png" alt="ChatterSqaure"/>
+                            <br/>
                             <h4 className="project-name">ChatterSquare</h4>
+                            <div className="overlay">
+                                <p className="overlay-text">
+                                A full-stack project that provides an online chatting platform to allow users to converse and befriend with other users in selected chat rooms 
+                                </p>
+                            </div>
                         </a>
                     </div>
-                    <div>
-                        <a href="/">
+                    <div className="project-item">
+                        <a className="project-link" href="https://github.com/ChuSong327/NunuNails-2.0" target="_blank" rel="noopener noreferrer">
                             <img className="project-image" src="https://res.cloudinary.com/chu327/image/upload/v1533710804/Nunu%20Nails/Screen_Shot_2018-08-07_at_11.45.54_PM.png" alt="Nunu Nails"/>
-                        </a>
-                        <br/>
-                        <a className="project-link" href="/">
+                            <br/>
                             <h4 className="project-name">Nunu Nails</h4>
+                            <div className="overlay">
+                                <p className="overlay-text">
+                                    A full-stack project
+                                </p>
+                            </div>
                         </a>
                     </div> 
                 </div>
