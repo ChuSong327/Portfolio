13b0f891d1e0da3c271f71bfcc6b41bd2da1d204
Chu Song <chuchusong23@gmail.com>
2018-08-12 08:02:33 -0700

    Install gh-pages and prepare the app to deploy on Github. Minor changes in style.

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -11,20 +11,22 @@ class Skills extends Component {
                 </h3>
                 <div className="skills-list">
                     <i className="skills-item devicon-javascript-plain colored"></i>
-                    <i class="skills-item devicon-typescript-plain colored"></i>
+                    <i className="skills-item devicon-typescript-plain colored"></i>
                     <i className="skills-item devicon-nodejs-plain colored"></i>
-                    <img className="skills-image" src="https://res.cloudinary.com/chu327/image/upload/v1533758068/portfolio/angular.png" alt="Angular"/>
+                    <i className="skills-item devicon-react-original-wordmark colored"></i>
+                    <img className="skills-image" src="https://res.cloudinary.com/chu327/image/upload/v1534085991/portfolio/angular_1.png" alt="mui"/>
                     <i className="skills-item devicon-html5-plain-wordmark colored"></i>
                     <i className="skills-item devicon-css3-plain-wordmark colored"></i>
                     <i className="skills-item devicon-sass-original colored"></i>
-                    <i className="skills-item devicon-react-original-wordmark colored"></i>
                     <i className="skills-item devicon-bootstrap-plain-wordmark colored"></i>
-                    <img className="skills-image" src="https://res.cloudinary.com/chu327/image/upload/v1533758783/portfolio/mui.png" alt="mui"/>
+                    <img className="skills-image" src="https://res.cloudinary.com/chu327/image/upload/v1534085925/portfolio/brand.png" alt="mui"/>
                     <i className="skills-item devicon-jquery-plain-wordmark colored"></i>
                     <i className="skills-item devicon-mysql-plain-wordmark colored"></i>
                     <i className="skills-item devicon-postgresql-plain-wordmark colored"></i>
                     <i className="skills-item devicon-mongodb-plain-wordmark colored"></i>
                     <i className="skills-item devicon-heroku-plain-wordmark colored"></i>
+                    <i className="skills-item devicon-github-plain colored"></i>
+                    <i className="skills-item devicon-amazonwebservices-plain-wordmark colored"></i>
                 </div>
             </div>
         )
